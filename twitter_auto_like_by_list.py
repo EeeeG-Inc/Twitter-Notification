3edd37125d1ea1dbf0cd7736c1600203ded4e891
Takuma Ikeda <takuma.ikeda@eeeeg.tokyo>
2022-08-16 17:16:14 +0900

    fix: 自然なかたちでリクエストを投げる

--- twitter_auto_like_by_list.py
+++ twitter_auto_like_by_list.py
@@ -1,6 +1,6 @@
 from my_twitter import MyTwitter
-import datetime
-from datetime import timedelta
+import random
+import time
 
 
 class TwitterAutoLikeByList:
@@ -10,13 +10,15 @@ class TwitterAutoLikeByList:
         # LIKE は 15 分以内で 50 回まで。それ以上実行すると 429 Too Many Requests がしばらく発生するので注意
         # NOTE: https://developer.twitter.com/en/docs/twitter-api/rate-limits
         api_limit = 50
-        max_results = 10
+        max_results = random.choice([12, 11, 10, 9, 8])
 
         tweets = twitter.get_list_tweets(twitter.config.auto_like_list_id, max_results)
 
         if len(tweets) <= api_limit:
+            time.sleep(random.choice([1, 2, 3]))
             twitter.like(tweets)
         else:
+            time.sleep(random.choice([1, 2, 3]))
             twitter.like(tweets[0:api_limit])
 
         print('Auto Like By List Done')
